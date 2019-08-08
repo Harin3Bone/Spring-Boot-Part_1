1a3cb651b4db09d128c9c71189ce161c4780ff26
Zchwazer <yamiyomuyumi@gmail.com>
2019-08-08 13:07:30 +0700

    test application properties
    input application info and enter application end point (actuator dependency allow)

--- src/main/java/com/example/practice/res/RestController.java
+++ src/main/java/com/example/practice/res/RestController.java
@@ -1,4 +1,32 @@
 package com.example.practice.res;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+
+@org.springframework.web.bind.annotation.RestController
 public class RestController {
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World "+ LocalDate.now();
+    }
+
+    // Expose new end point
+    @GetMapping("/thanks")
+    public String sayThankyou(){
+        return "Thank you";
+    }
+
+    // Expose new end point
+    @GetMapping("/bye")
+    public String sayGoodBye(){
+        return "Good Bye, See you again";
+    }
+
+    // Expose new end point
+    @GetMapping("/test")
+    public String sayTest(){
+        return "Test Test Test";
+    }
 }
